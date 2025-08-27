76483245435ccefaed38ba542d7d762efa823664
Tyler Scott <ty@lerscott.com>
2025-08-27 11:04:02 -0400

    test pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/trase-challenge/",
   plugins: [
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
     }),
-    react()],
+    react()
+  ],
+  test: {
+    globals: true
+  },
 })
