61a7963dfa1063103099dcffe2489a15e0397e5a
Tyler Scott <ty@lerscott.com>
2025-08-27 11:14:59 -0400

    test deploy with correct base path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, basepath: '/trase-challenge/' })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
